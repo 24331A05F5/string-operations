ce19f1fc1cd179e4ca0de9c6ab9126315ce61e68
24331A05F5 <kundhetiuma@gmail.com>
2026-03-29 15:29:03 +0530

    Add files via upload

---
+++ wf1.py
@@ -0,0 +1,4 @@
+s=input("enter a string to find letter occurence:")
+ch=input("enter a letter:")
+c=s.count(ch)
+print(ch,"is repeated in",s,"string",c,"times")
---
+++ wf3.py
@@ -0,0 +1,3 @@
+string = input("Enter a string to replace all occurrences of 'a' with 'x': ")
+new_string = string.replace('a', 'x')
+print("Modified string:", new_string)
